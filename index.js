a7606610b0325534de54e67571478b82114d113a
Manuel Garat <m.garat@outlook.com>
2022-04-10 01:52:22 -0300

    fix Crear back-end para comunicar los front-end con la base de datos #13

--- index.js
+++ index.js
@@ -1,37 +1,143 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
+var sqlite3 = require("sqlite3").verbose();
+
+// ---------------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------- frontend routes --------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
 
 app.use(cors());
-app.use('/admin', express.static(path.join(__dirname, './src/admin/build')))
-app.use('/visualization', express.static(path.join(__dirname, './src/visualization/build')))
-app.use('/', express.static(path.join(__dirname, './src/visualization/build')))
+app.use(express.json());
+app.use("/admin", express.static(path.join(__dirname, "./src/admin/build")));
 
-app.get('admin/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/src/admin/build/index.html'))
-})
+app.use(
+  "/visualization",
+  express.static(path.join(__dirname, "./src/visualization/build"))
+);
 
-app.get('visualization/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/src/visualization/build/index.html'))
-})
+app.use("/", express.static(path.join(__dirname, "./src/visualization/build")));
+
+app.get("/admin/*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/src/admin/build/index.html"));
+});
+
+app.get("/visualization/*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/src/visualization/build/index.html"));
+});
 
 // replace next endpoint with actual backend code
 
-app.get('/backend/hello-world', (req, res) => {
-    res.send('Hello World!')
+// ---------------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------ database endpoints -------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
+
+// ------------------------------------------------ database connection ------------------------------------------------
+
+var db = new sqlite3.Database("db/dcic-schedule.db");
+db.serialize();
+
+// ---------------------------------------------------- room-event -----------------------------------------------------
+
+app.get("/backend/room-event", (req, res) => {
+  db.all("select * from room_event", [], (err, rows) => {
+    if (err) res.status(400).json({ error: err.message });
+    else res.status(200).json(rows);
+  });
 });
+
+app.get("/backend/room-event/:id", (req, res) => {
+  db.get(
+    "select * from room_event where id=" + req.params.id,
+    [],
+    (err, rows) => {
+      if (err) res.status(400).json({ error: err.message });
+      else res.status(200).json(rows);
+    }
+  );
+});
+
+// --------------------------------------------------- announcement ----------------------------------------------------
+
+app.get("/backend/announcement", (req, res) => {
+  db.all("select * from announcement", [], (err, rows) => {
+    if (err) res.status(400).json({ error: err.message });
+    else res.status(200).json(rows);
+  });
+});
+
+app.get("/backend/announcement/:id", (req, res) => {
+  db.get(
+    "select * from announcement where id=" + req.params.id,
+    [],
+    (err, rows) => {
+      if (err) res.status(400).json({ error: err.message });
+      else res.status(200).json(rows);
+    }
+  );
+});
+
+app.post("/backend/announcement", (req, res) => {
+  db.run(
+    "insert into announcement (message,writer,priority,timestamp_begin,timestamp_end,timestamp_create,timestamp_update) values ($message,$writer,$priority,$timestamp_begin,$timestamp_end,$timestamp_create,$timestamp_update)",
+    {
+      $message: req.body.message,
+      $writer: req.body.writer,
+      $priority: req.body.priority,
+      $timestamp_begin: req.body.timestamp_begin,
+      $timestamp_end: req.body.timestamp_end,
+      $timestamp_create: req.body.timestamp_create,
+      $timestamp_update: req.body.timestamp_update,
+    },
+    (err) => {
+      if (err) res.status(400).json({ error: err.message });
+      else res.status(200).json({ result: "updated!" });
+    }
+  );
+});
+
+app.put("/backend/announcement", (req, res) => {
+  db.run(
+    "update announcement set message=$message, writer=$writer,priority=$priority,timestamp_begin=$timestamp_begin,timestamp_end=$timestamp_end,timestamp_create=$timestamp_create,timestamp_update=$timestamp_update where id=$id",
+    {
+      $id: req.body.id,
+      $message: req.body.message,
+      $writer: req.body.writer,
+      $priority: req.body.priority,
+      $timestamp_begin: req.body.timestamp_begin,
+      $timestamp_end: req.body.timestamp_end,
+      $timestamp_create: req.body.timestamp_create,
+      $timestamp_update: req.body.timestamp_update,
+    },
+    (err) => {
+      if (err) res.status(400).json({ error: err.message });
+      else res.status(200).json({ result: "created!" });
+    }
+  );
+});
+
+app.delete("/backend/announcement/:id", (req, res) => {
+  db.run("delete from announcement where id=" + req.params.id, [], (err) => {
+    if (err) res.status(400).json({ error: err.message });
+    else res.status(200).json({ result: "deleted!" });
+  });
+});
+
+// ---------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------- the rest ------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
 
 // map all other endpoints to visualization
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/src/visualization/build/index.html'))
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/src/visualization/build/index.html"));
+});
 
 // start listening
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`App listening on http://127.0.0.1:${PORT}`);
-    console.log('Press Ctrl+C to quit.');
+  console.log(`App listening on http://127.0.0.1:${PORT}`);
+  console.log("Press Ctrl+C to quit.");
 });
