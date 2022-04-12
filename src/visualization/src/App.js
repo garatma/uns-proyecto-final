484e351e48bad9629b4b2f8fb88dae5f0ba4b7de
Manuel Garat <m.garat@outlook.com>
2022-04-12 20:49:48 -0300

    Third test for heroku auto deploy

--- src/visualization/src/App.js
+++ src/visualization/src/App.js
@@ -95,7 +95,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React for visualization (testing 2)
+          Learn React for visualization (testing 3)
         </a>
         <button onClick={GetFromRoomEvent}>Obtener info de room-event</button>
         <button onClick={GetFromAnnouncement}>
