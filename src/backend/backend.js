6ac5cdd930a248a6c9de0302ec99a6f787f4b348
Manuel Garat <m.garat@outlook.com>
2022-04-29 20:13:55 -0300

    Fix warnings + launch.json for vscode

--- src/backend/backend.js
+++ src/backend/backend.js
@@ -12,19 +12,16 @@ app.use(cors());
 app.use(express.json());
 app.use("/admin", express.static(path.join(__dirname, "../admin/build")));
 
-app.use(
-  "/visualization",
-  express.static(path.join(__dirname, "../visualization/build"))
-);
+app.use("/visualization", express.static(path.join(__dirname, "../visualization/build")));
 
 app.use("/", express.static(path.join(__dirname, "../visualization/build")));
 
-app.get("/admin/*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/../admin/build/index.html"));
+app.get("/admin/*", (res) => {
+    res.sendFile(path.join(__dirname + "/../admin/build/index.html"));
 });
 
-app.get("/visualization/*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/../visualization/build/index.html"));
+app.get("/visualization/*", (res) => {
+    res.sendFile(path.join(__dirname + "/../visualization/build/index.html"));
 });
 
 // replace next endpoint with actual backend code
@@ -40,89 +37,80 @@ db.serialize();
 
 // ---------------------------------------------------- room-event -----------------------------------------------------
 
-app.get("/backend/room-event", (req, res) => {
-  db.all("select * from room_event", [], (err, rows) => {
-    if (err) res.status(400).json({ error: err.message });
-    else res.status(200).json(rows);
-  });
+app.get("/backend/room-event", (res) => {
+    db.all("select * from room_event", [], (err, rows) => {
+        if (err) res.status(400).json({ error: err.message });
+        else res.status(200).json(rows);
+    });
 });
 
 app.get("/backend/room-event/:id", (req, res) => {
-  db.get(
-    "select * from room_event where id=" + req.params.id,
-    [],
-    (err, rows) => {
-      if (err) res.status(400).json({ error: err.message });
-      else res.status(200).json(rows);
-    }
-
-  );
+    db.get("select * from room_event where id=" + req.params.id, [], (err, rows) => {
+        if (err) res.status(400).json({ error: err.message });
+        else res.status(200).json(rows);
+    });
 });
 
 // --------------------------------------------------- announcement ----------------------------------------------------
 
-app.get("/backend/announcement", (req, res) => {
-  db.all("select * from announcement", [], (err, rows) => {
-    if (err) res.status(400).json({ error: err.message });
-    else res.status(200).json(rows);
-  });
+app.get("/backend/announcement", (res) => {
+    db.all("select * from announcement", [], (err, rows) => {
+        if (err) res.status(400).json({ error: err.message });
+        else res.status(200).json(rows);
+    });
 });
 
 app.get("/backend/announcement/:id", (req, res) => {
-  db.get(
-    "select * from announcement where id=" + req.params.id,
-    [],
-    (err, rows) => {
-      if (err) res.status(400).json({ error: err.message });
-      else res.status(200).json(rows);
-    }
-  );
+    db.get("select * from announcement where id=" + req.params.id, [], (err, rows) => {
+        if (err) res.status(400).json({ error: err.message });
+        else res.status(200).json(rows);
+    });
 });
 
 app.post("/backend/announcement", (req, res) => {
-  db.run(
-    "insert into announcement (message,writer,priority,timestamp_begin,timestamp_end,timestamp_create,timestamp_update) values ($message,$writer,$priority,$timestamp_begin,$timestamp_end,$timestamp_create,$timestamp_update)",
-    {
-      $message: req.body.message,
-      $writer: req.body.writer,
-      $priority: req.body.priority,
-      $timestamp_begin: req.body.timestamp_begin,
-      $timestamp_end: req.body.timestamp_end,
-      $timestamp_create: req.body.timestamp_create,
-      $timestamp_update: req.body.timestamp_update,
-    },
-    (err) => {
-      if (err) res.status(400).json({ error: err.message });
-      else res.status(200).json({ result: "created!" });
-    }
-  );
+    db.run(
+        "insert into announcement (message,writer,priority,timestamp_begin,timestamp_end,timestamp_create,timestamp_update) values ($message,$writer,$priority,$timestamp_begin,$timestamp_end,$timestamp_create,$timestamp_update)",
+        {
+            $message: req.body.message,
+            $writer: req.body.writer,
+            $priority: req.body.priority,
+            $timestamp_begin: req.body.timestamp_begin,
+            $timestamp_end: req.body.timestamp_end,
+            $timestamp_create: req.body.timestamp_create,
+            $timestamp_update: req.body.timestamp_update
+        },
+        (err) => {
+            if (err) res.status(400).json({ error: err.message });
+            else res.status(200).json({ result: "created!" });
+        }
+    );
 });
 
 app.put("/backend/announcement", (req, res) => {
-  db.run(
-    "update announcement set message=$message, writer=$writer,priority=$priority,timestamp_begin=$timestamp_begin,timestamp_end=$timestamp_end,timestamp_create=$timestamp_create,timestamp_update=$timestamp_update where id=$id",
-    {
-      $id: req.body.id,
-      $message: req.body.message,
-      $writer: req.body.writer,
-      $priority: req.body.priority,
-      $timestamp_begin: req.body.timestamp_begin,
-      $timestamp_end: req.body.timestamp_end,
-      $timestamp_create: req.body.timestamp_create,
-      $timestamp_update: req.body.timestamp_update,
-    },
-    (err) => {
-      if (err) res.status(400).json({ error: err.message });
-      else res.status(200).json({ result: "updated!" });
-    }
-  );
+    db.run(
+        "update announcement set message=$message, writer=$writer,priority=$priority,timestamp_begin=$timestamp_begin,timestamp_end=$timestamp_end,timestamp_create=$timestamp_create,timestamp_update=$timestamp_update where id=$id",
+        {
+            $id: req.body.id,
+            $message: req.body.message,
+            $writer: req.body.writer,
+            $priority: req.body.priority,
+            $timestamp_begin: req.body.timestamp_begin,
+            $timestamp_end: req.body.timestamp_end,
+            $timestamp_create: req.body.timestamp_create,
+            $timestamp_update: req.body.timestamp_update
+        },
+        (err) => {
+            if (err) res.status(400).json({ error: err.message });
+            else res.status(200).json({ result: "updated!" });
+        }
+    );
 });
 
 app.delete("/backend/announcement/:id", (req, res) => {
-  db.run("delete from announcement where id=" + req.params.id, [], (err) => {
-    if (err) res.status(400).json({ error: err.message });
-    else res.status(200).json({ result: "deleted!" });
-  });
+    db.run("delete from announcement where id=" + req.params.id, [], (err) => {
+        if (err) res.status(400).json({ error: err.message });
+        else res.status(200).json({ result: "deleted!" });
+    });
 });
 
 // ---------------------------------------------------------------------------------------------------------------------
@@ -131,14 +119,14 @@ app.delete("/backend/announcement/:id", (req, res) => {
 
 // map all other endpoints to visualization
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/../visualization/build/index.html"));
+app.get("*", (res) => {
+    res.sendFile(path.join(__dirname + "/../visualization/build/index.html"));
 });
 
 // start listening
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`App listening on http://127.0.0.1:${PORT}`);
-  console.log("Press Ctrl+C to quit.");
+    console.log(`App listening on http://127.0.0.1:${PORT}`);
+    console.log("Press Ctrl+C to quit.");
 });
