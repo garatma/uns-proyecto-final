3a918f3b24bc44944700311c0c90ee3c2f0225bf
Manuel Garat <m.garat@outlook.com>
2022-05-25 22:03:00 -0300

    Admin front-end almost ready
    We only have to post the form to the backend and that's it

--- src/admin/src/App.js
+++ src/admin/src/App.js
@@ -1,163 +1,93 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 
-async function GetFromRoomEvent() {
-    // just a sample get fetch to show how to pass a url for the backend
-    fetch("/backend/room-event")
-        .then((response) => response.text())
-        .then((texto) => alert(texto))
-        .catch((razon) => alert("no se pudo hacer el request: " + razon));
-}
-
-async function GetFromAnnouncement() {
-    // just a sample get fetch to show how to pass a url for the backend
-    var id = "";
-    fetch("/backend/announcement/" + id)
-        .then((response) => response.text())
-        .then((texto) => alert(texto))
-        .catch((razon) => alert("no se pudo hacer el request: " + razon));
-}
-
-async function Post() {
-    const insert = {
-        message: "A blog post by Kingsley",
-        writer: "Brilliant post on fetch API",
-        priority: "dsafa",
-        timestamp_begin: 0,
-        timestamp_end: 0,
-        timestamp_create: 0,
-        timestamp_update: 0
-    };
-
-    const options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(insert)
-    };
-    // just a sample get fetch to show how to pass a url for the backend
-    fetch("/backend/announcement/", options)
-        .then((response) => response.text())
-        .then((texto) => alert(texto))
-        .catch((razon) => alert("no se pudo hacer el request: " + razon));
-}
-
-async function Put() {
-    const update = {
-        id: 7,
-        message: "sffdsada",
-        writer: "Brilliant post on fetch API",
-        priority: "dsafa",
-        timestamp_begin: 0,
-        timestamp_end: 0,
-        timestamp_create: 0,
-        timestamp_update: 0
-    };
-
-    const options = {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(update)
-    };
-    // just a sample get fetch to show how to pass a url for the backend
-    fetch("/backend/announcement/", options)
-        .then((response) => response.text())
-        .then((texto) => alert(texto))
-        .catch((razon) => alert("no se pudo hacer el request: " + razon));
-}
-
-async function Delete() {
-    var id = "2";
-    const options = {
-        method: "DELETE"
-    };
-    // just a sample get fetch to show how to pass a url for the backend
-    fetch("/backend/announcement/" + id, options)
-        .then((response) => response.text())
-        .then((texto) => alert(texto))
-        .catch((razon) => alert("no se pudo hacer el request: " + razon));
-}
-
-class Date extends React.Component {
+class Timestamp extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            startDate: 0
-        };
-
+        console.log(props);
         this.handleInputChange = this.handleInputChange.bind(this);
     }
 
     handleInputChange(event) {
-        this.setState({ startDate: event.target.value });
-        console.log(event.target.value);
+        this.props.onChange(event.target.name, event.target.value);
     }
 
-    render() {
-        return <input className="date" type="date" value={this.state.startDate} onChange={this.handleInputChange} />;
-    }
-}
-
-class Time extends React.Component {
-    render() {
-        return (
-            <div>
-                <Date />
-                <Number min={0} max={23} />
-                :
-                <Number min={0} max={59} />
-                hs
-            </div>
-        );
-    }
-}
-
-class Number extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            number: 0
-        };
-
-        this.handleInputChange = this.handleInputChange.bind(this);
-    }
-
-    handleInputChange(event) {
-        let value = event.target.value;
-        if (value < this.props.min || value > this.props.max) return;
-        this.setState({
-            number: parseInt(value)
-        });
-    }
     render() {
         return (
             <input
-                className="hourAndMinutes"
-                type="number"
-                min={this.props.min}
-                max={this.props.max}
-                value={this.state.number}
+                required
+                value={this.props.value}
+                name={this.props.name}
+                type="datetime-local"
                 onChange={this.handleInputChange}
-            />
+            ></input>
         );
     }
 }
 
+class Priority extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        this.props.onChange(event.target.value);
+    }
+
+    render() {
+        return (
+            <select value={this.props.value} className="priority" onChange={this.handleInputChange}>
+                <option value="LOW">Baja</option>
+                <option value="MID">Media</option>
+                <option value="HIGH">Alta</option>
+            </select>
+        );
+    }
+}
 
 class Announcement extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             title: "",
-            textAnnouncement: "",
+            body: "",
+            fromTimestamp: this.now(),
+            toTimestamp: this.now(),
+            priority: "MID",
             author: ""
         };
 
+        console.log(this.state);
+
+        this.handleTimestampChange = this.handleTimestampChange.bind(this);
         this.handleTextChange = this.handleTextChange.bind(this);
+        this.handlePriorityChange = this.handlePriorityChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    now() {
+        let now = new Date();
+
+        let year = now.getFullYear();
+        let month = now.getMonth() < 10 ? "0" + now.getMonth() : now.getMonth() + 1;
+        let day = now.getDate() < 10 ? "0" + now.getDate() : now.getDate();
+        let hours = now.getHours() < 10 ? "0" + now.getHours() : now.getHours();
+        let minutes = now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes();
+
+        return year + "-" + month + "-" + day + "T" + hours + ":" + minutes;
+    }
+
+    handleTimestampChange(name, value) {
+        if (name === "fromTimestamp") {
+            this.setState({
+                fromTimestamp: value
+            });
+        } else {
+            this.setState({
+                toTimestamp: value
+            });
+        }
     }
 
     handleTextChange(event) {
@@ -169,31 +99,72 @@ class Announcement extends React.Component {
         });
     }
 
+    handlePriorityChange(value) {
+        this.setState({
+            priority: value
+        });
+    }
+
+    handleSubmit(event) {
+        console.log(this.state);
+        // TODO: fetch post to backend with this format:
+        // {
+        //     $message: req.body.message,
+        //     $writer: req.body.writer,
+        //     $priority: req.body.priority,
+        //     $timestamp_begin: req.body.timestamp_begin,
+        //     $timestamp_end: req.body.timestamp_end,
+        //     $timestamp_create: req.body.timestamp_create,
+        //     $timestamp_update: req.body.timestamp_update
+        // },
+        // we have to transform the string timestamps to int (epoch) timestamps
+        // TODO: the table should have a column for the title of the announcement
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <label>Título:</label>
-                    <input required name="title" type="text" value={this.state.title} onChange={this.handleTextChange} />
+                    <input
+                        required
+                        name="title"
+                        type="text"
+                        value={this.state.title}
+                        onChange={this.handleTextChange}
+                    />
+
                     <label>Anuncio:</label>
-                    <textarea required name="textAnnouncement" value={this.state.textAnnouncement} onChange={this.handleTextChange} ></textarea>
-                    <label>
-                        Desde:
-                        <Time />
-                    </label>
-                    <label>
-                        Hasta:
-                        <Time />
-                    </label>
+                    <textarea required name="body" value={this.state.body} onChange={this.handleTextChange}></textarea>
+
+                    <label>Desde:</label>
+                    <Timestamp
+                        value={this.state.fromTimestamp}
+                        name="fromTimestamp"
+                        onChange={this.handleTimestampChange}
+                    />
+
+                    <label>Hasta:</label>
+                    <Timestamp
+                        value={this.state.toTimestamp}
+                        name="toTimestamp"
+                        onChange={this.handleTimestampChange}
+                    />
+
                     <label>Prioridad:</label>
+                    <Priority onChange={this.handlePriorityChange} value={this.state.priority} />
+
                     <label className="authorLabel">Autor/a:</label>
-                    <input required className="authorInput" name="author" type="text" value={this.state.author} onChange={this.handleTextChange} />
-                    <button onClick={() => {
-                        console.log(this.state.title);
-                        console.log(this.state.title);
-                        console.log(this.state.textAnnouncement);
-                        console.log(this.state.author);
-                    }}>Publicar Anuncio</button>
+                    <input
+                        required
+                        name="author"
+                        type="text"
+                        value={this.state.author}
+                        onChange={this.handleTextChange}
+                    />
+
+                    <button>Publicar anuncio</button>
                 </form>
             </div>
         );
@@ -207,7 +178,7 @@ class App extends React.Component {
                 <header className="App-header">
                     <Announcement></Announcement>
                 </header>
-            </div >
+            </div>
         );
     }
 }
