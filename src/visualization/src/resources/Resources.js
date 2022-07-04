603b00c83fafb2fe034208c7f81c7ab5eb517182
Bárbara Mattioli <barbaramattioli@outlook.com>
2022-07-04 18:34:25 -0300

    Change of icons to make the default color the same for all

--- src/visualization/src/resources/Resources.js
+++ src/visualization/src/resources/Resources.js
@@ -2,7 +2,7 @@ import projectorIcon from "./icons/projector.png";
 import soundIcon from "./icons/sound.png";
 import disabledAccessIcon from "./icons/disabledAccess.png";
 import wifiIcon from "./icons/wifi.png";
-import ethernetIcon from "./icons/ethernet3.png";
+import ethernetIcon from "./icons/ethernet.png";
 //<a href="https://www.flaticon.com/free-icons/ethernet" title="ethernet icons">Ethernet icons created by Pixelmeetup - Flaticon</a>
 
 function Resources(props) {
